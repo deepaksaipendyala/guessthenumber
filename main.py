2f815ea589326e00623f9bccff9ba499f79c40f9
Deepak sai Pendyala <>
2021-06-03 04:54:53 +0000

    formatted

--- main.py
+++ main.py
@@ -7,30 +7,31 @@
 # For a little added challenge, you can limit the number of guesses to 5, for example.
 # (required library "random")
 
-
 #code:
-i=1
-while i>0:
-  import random
-  a=random.randint(0,100)
-  print('you will have only 5 chances to guess ')
-  chance=0
-  while chance<5:
-    guess=int(input("enter the number:"))
-    if guess>a:
-      print(f"you are too high, u can try again in {4-chance} chances")
-    elif guess<a:
-      print(f"you are too low, u can try again in {4-chance} chances")
-    elif guess==a:
-      print("Ohh yeah! You got it")
-      cont=input("enter 'yes' if u wanna play again \n else enter 'no' to exit:")
-      if cont=='no':
-        break
-      if cont=='yes':
-        i=1
-        break
-    chance+=1
-    if chance==5:
-      print('you are out of chances. sorry!')
-    i=0
+i = 1
+while i > 0:
+    import random
+    a = random.randint(0, 100)
+    print('you will have only 5 chances to guess ')
+    chance = 0
+    while chance < 5:
+        guess = int(input("enter the number:"))
+        if guess > a:
+            print(f"you are too high, u can try again in {4-chance} chances")
+        elif guess < a:
+            print(f"you are too low, u can try again in {4-chance} chances")
+        elif guess == a:
+            print("Ohh yeah! You got it")
+            cont = input(
+                "enter 'yes' if u wanna play again \n else enter 'no' to exit:"
+            )
+            if cont == 'no':
+                break
+            if cont == 'yes':
+                i = 1
+                break
+        chance += 1
+        if chance == 5:
+            print('you are out of chances. sorry!')
+        i = 0
 #End.
